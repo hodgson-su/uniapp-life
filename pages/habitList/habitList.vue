3ba4c0798db7149b93062aa3b2f265b9203be301
hodgson <htz4004@126.com>
2020-06-28 18:03:24 +0800

    1803

--- pages/habitList/habitList.vue
+++ pages/habitList/habitList.vue
@@ -39,7 +39,7 @@
 							},
 							{
 								'id':1003,
-								'name':'早睡',
+								'name':'喝充足的水',
 								'timestamp':{},
 								'numday':0,
 								'now_sub_id':1
@@ -53,13 +53,83 @@
 							},
 							{
 								'id':1005,
-								'name':'规律睡眠',
+								'name':'排便',
 								'timestamp':{},
 								'numday':0,
 								'now_sub_id':1
 							}
 						]
-					}]
+					},
+					{
+						'id':2,
+						'name':'运动',
+						'habitList':[
+							{
+								'id':2000,
+								'name':'初级瑜伽',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2001,
+								'name':'中级瑜伽',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2002,
+								'name':'高级瑜伽',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2003,
+								'name':'生理期瑜伽',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2004,
+								'name':'跑步',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2005,
+								'name':'散步',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2006,
+								'name':'初级HIIT',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2007,
+								'name':'中级HIIT',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							},
+							{
+								'id':2008,
+								'name':'高级HIIT',
+								'timestamp':{},
+								'numday':0,
+								'now_sub_id':1
+							}
+						]
+					}
+					]
 			}
 		},
 		methods: {
